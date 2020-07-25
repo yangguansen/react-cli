555eacfef17f2489fe4af4850ff006e4b9ca8100
123 <416650596@qq.com>
2020-07-25 20:13:37 +0800

    submit somethins

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import HomePage from "./home/index";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import ExpensePage from "./expense";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<a href="/home">to home</a>
+			<br/>
+			<a href="/expense">to expense</a>
+			<Router>
+				<Switch>
+					<Route path="/home" component={HomePage}></Route>
+					<Route path="/expense" component={ExpensePage}></Route>
+				</Switch>
+
+			</Router>
+		</div>
+
+
+	);
 }
 
 export default App;
---
+++ src/expense/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class ExpensePage extends React.Component {
+	render() {
+		return <div>Expense Page</div>;
+	}
+}
+
+export default ExpensePage;
